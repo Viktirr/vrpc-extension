01e25ad9561bc75fea4a0f0c514a59e1ae009559
Viktir <71885195+Viktirr@users.noreply.github.com>
2024-12-27 18:06:21 +0100

    Added text scrolling on overflown rich presence data [Popup]

--- popup/script.js
+++ popup/script.js
@@ -1,3 +1,6 @@
+let isRPCRunning = false;
+let isScrollRunning = false;
+
 function GetRichPresence() {
     console.log("[Popup] Get RPC Info")
     browser.runtime.sendMessage({
@@ -24,8 +27,7 @@ function sleep(ms) {
 }
 
 isRegularCheckForRichPresenceRunning = false;
-async function RegularlyCheckForRichPresence()
-{
+async function RegularlyCheckForRichPresence() {
     if (isRegularCheckForRichPresenceRunning == true) { return; }
     while (true) {
         isRegularCheckForRichPresenceRunning = true;
@@ -35,6 +37,58 @@ async function RegularlyCheckForRichPresence()
             content: "GET_RPC_INFO"
         });
     }
+}
+
+
+function startScrollRichPresenceText(element, container) {
+    let speed = 0.8;
+    let position = 0;
+    let directionRight = true;
+    let _directionRight = true;
+    let idleIteration = 0;
+    let idleIterationCap = 120;
+
+    function scrollRichPresenceText() {
+        if (isRPCRunning == false) { isScrollRunning = false; return; }
+
+        const containerWidth = container.offsetWidth;
+        const textWidth = element.offsetWidth;
+
+        if (directionRight != _directionRight) {
+            idleIteration += 1;
+            _directionRight = directionRight;
+        }
+
+        if (textWidth > containerWidth) {
+            if (idleIteration != 0) {
+                idleIteration += 1;
+                if (idleIteration >= idleIterationCap) {
+                    idleIteration = 0;
+                }
+            }
+            else if ((position + textWidth) >= containerWidth && directionRight == true) {
+                position -= speed;
+            }
+            else if (position < 0) {
+                directionRight = false;
+                position += speed;
+            } else {
+                position = 0;
+                directionRight = true;
+                idleIteration += 1;
+            }
+        } else {
+            position = 0;
+        }
+
+        element.style.transform = `translateX(${position}px)`;
+
+        fps = 60;
+        setTimeout(() => {
+            requestAnimationFrame(scrollRichPresenceText);
+        }, 1000 / fps);
+    }
+    scrollRichPresenceText();
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -47,7 +101,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 browser.runtime.onMessage.addListener((message) => {
     if (message.type === "SEND_POPUP_INFO") {
-        console.log("[Popup] Received: " + message.content);
+        //console.log("[Popup] Received: " + message.content);
         if (message.content === "STATUS: ALIVE") {
             document.getElementById("app-status").classList.add("success");
             document.getElementById("app-status-text").innerHTML = "Connected";
@@ -63,28 +117,37 @@ browser.runtime.onMessage.addListener((message) => {
             document.getElementsByClassName("rich-presence-text-first")[0].innerHTML = currentrpcinfo[0];
             document.getElementsByClassName("rich-presence-text-second")[0].innerHTML = currentrpcinfo[1];
             document.getElementsByClassName("rich-presence-text-third")[0].innerHTML = currentrpcinfo[2];
-            document.getElementsByClassName("rich-presence-image")[0].setAttribute("src",currentrpcinfo[3]);
-            document.getElementsByClassName("rich-presence-image")[0].setAttribute("title",currentrpcinfo[2]);
+            document.getElementsByClassName("rich-presence-image")[0].setAttribute("src", currentrpcinfo[3]);
+            document.getElementsByClassName("rich-presence-image")[0].setAttribute("title", currentrpcinfo[2]);
 
             if (currentrpcinfo[4].includes("True")) {
+                isRPCRunning = true;
                 document.getElementsByClassName("rich-presence-status-text")[0].innerHTML = "Rich presence is active.";
                 document.getElementById("rich-presence-container").classList.add("active");
                 document.getElementById("rich-presence-container").classList.remove("inactive");
+
+                if (isScrollRunning == false)
+                {
+                    isScrollRunning = true;
+                    startScrollRichPresenceText(document.querySelector(".rich-presence-text-first"), document.querySelector(".rich-presence-text-container"));
+                    startScrollRichPresenceText(document.querySelector(".rich-presence-text-second"), document.querySelector(".rich-presence-text-container"));
+                    startScrollRichPresenceText(document.querySelector(".rich-presence-text-third"), document.querySelector(".rich-presence-text-container"));
+                }
             }
             else {
+                isRPCRunning = false;
                 document.getElementsByClassName("rich-presence-status-text")[0].innerHTML = "Rich presence is inactive.";
                 document.getElementById("rich-presence-container").classList.add("inactive");
                 document.getElementById("rich-presence-container").classList.remove("active");
             }
-            
+
             let currentTime = Math.floor(Date.now() / 1000);
             let songDuration = parseInt(currentrpcinfo[6]) - parseInt(currentrpcinfo[5]);
-            
+
             document.getElementsByClassName("rich-presence-time-start")[0].innerHTML = ConvertIntoTimestamp(currentTime - currentrpcinfo[5]);
             document.getElementsByClassName("rich-presence-time-end")[0].innerHTML = ConvertIntoTimestamp(songDuration);
 
-            let percentage = (((currentTime - currentrpcinfo[5]) - 0)/(songDuration - 0) * 100);
-            console.log(percentage);
+            let percentage = (((currentTime - currentrpcinfo[5]) - 0) / (songDuration - 0) * 100);
 
             document.querySelector(".rich-presence-time-graphics-elapsed").style.width = percentage + "%";
         }
